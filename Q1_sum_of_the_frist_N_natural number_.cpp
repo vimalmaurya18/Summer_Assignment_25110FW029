e7d3ceb0f1a23a520152a1f093307582e8134ac0
vimalmaurya18 <vm5800034@gmail.com>
2026-06-02 16:45:31 +0530

    Update Q1_sum_of_the_frist_N_natural number_.cpp

--- Q1_sum_of_the_frist_N_natural number_.cpp
+++ Q1_sum_of_the_frist_N_natural number_.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 int main()
 {
- cout<<"my name is vimal maurya";
+  int sum=0,n;
+ cout<<"Enter the number upto the sum is to be calculated:";
+ cin>>n;
+ for(int i=1;i<=n;i++)
+  {
+   sum=sum+i;
+  }
+ cout<<"The sum of the n natural nummbers is:"<<sum;
  return 0;
 }
