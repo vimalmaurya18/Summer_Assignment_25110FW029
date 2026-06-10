729509ddaf2812c7b032b9512f5e8b6a5410fe4b
Vimal <vm5800034@gmail.com>
2026-06-10 17:47:47 +0530

    Day 10

---
+++ Q37_printing_star_pyramid_.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cout<<"enter the number of rows:";
+  cin>>n;
+  for(int i=0;i<n;i++)
+  {
+    for(int k=n-i;k>0;k--)
+    {
+      cout<<" ";
+    }
+      for(int j=0;j<(2*i+1);j++)
+      {
+        cout<<"*";
+      }
+      cout<<endl;
+  }
+  return 0;
+}
---
+++ Q38_printing_reverse_pyramid_.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cout<<"enter the number of rows:";
+  cin>>n;
+  for(int i=n;i>0;i--)
+  {
+    for(int k=0;k<n-i;k++)
+    {
+      cout<<" ";
+    }
+      for(int j=(2*i-1);j>0;j--)
+      {
+        cout<<"*";
+      }
+      cout<<endl;
+  }
+  return 0;
+}
